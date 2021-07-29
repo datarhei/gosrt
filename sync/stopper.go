3550b40a6054d005e00199a50755aa2ae3fee952
Ingo Oppermann <ingo@oppermann.ch>
2021-07-29 18:09:03 +0200

    Re-implement with sync primitives

--- sync/stopper.go
+++ sync/stopper.go
@@ -2,47 +2,54 @@
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
+// Package sync provides synchronisation tools
 package sync
 
-const (
-	stopperStop int = 1
-	stopperDone int = 2
-)
+import "sync"
 
 type Stopper interface {
-	Check() <-chan int
+	// Check returns a channel that's closed when Stop() has been called.
+	Check() <-chan struct{}
+
+	// Stop will close the Check() channel and then wait until Done() has been called.
 	Stop()
+
+	// Done will release the waiting Stop() call. You should not call
+	// this function before Stop() has been called.
 	Done()
 }
 
 type stopper struct {
-	c chan int
-	d chan int
+	channel chan struct{}
+	wait    sync.WaitGroup
+	stop    sync.Once
+	done    sync.Once
 }
 
+// NewStopper returns a new Stopper. A Stopper can't be reused after
+// Stop() has been called.
 func NewStopper() Stopper {
 	s := &stopper{
-		c: make(chan int),
-		d: make(chan int),
+		channel: make(chan struct{}),
 	}
 
 	return s
 }
 
-func (s *stopper) Check() <-chan int {
-	return s.c
+func (s *stopper) Check() <-chan struct{} {
+	return s.channel
 }
 
 func (s *stopper) Stop() {
-	s.c <- stopperStop
-
-	for n := range s.d {
-		if n == stopperDone {
-			return
-		}
-	}
+	s.stop.Do(func() {
+		s.wait.Add(1)
+		close(s.channel)
+		s.wait.Wait()
+	})
 }
 
 func (s *stopper) Done() {
-	s.d <- stopperDone
+	s.done.Do(func() {
+		s.wait.Done()
+	})
 }
