ac29b0a538572d8755a66afd470d6f5d15e7e1e7
Ingo Oppermann <ingo@datarhei.com>
2024-08-28 14:20:03 +0200

    Fix sending ACK for a lower sequence number than before (#67)

--- congestion/live/receive_test.go
+++ congestion/live/receive_test.go
@@ -252,6 +252,7 @@ func TestRecvACK(t *testing.T) {
 	require.Equal(t, uint32(0), seqNAKTo)
 	require.Equal(t, uint32(4), recv.maxSeenSequenceNumber.Val())
 	require.Equal(t, uint32(0), recv.lastACKSequenceNumber.Inc().Val())
+	require.Equal(t, uint32(0), recv.lastDeliveredSequenceNumber.Inc().Val())
 	require.Exactly(t, []uint32{}, numbers)
 
 	for i := 7; i < 10; i++ {
@@ -267,6 +268,7 @@ func TestRecvACK(t *testing.T) {
 	require.Equal(t, uint32(6), seqNAKTo)
 	require.Equal(t, uint32(9), recv.maxSeenSequenceNumber.Val())
 	require.Equal(t, uint32(0), recv.lastACKSequenceNumber.Inc().Val())
+	require.Equal(t, uint32(0), recv.lastDeliveredSequenceNumber.Inc().Val())
 	require.Exactly(t, []uint32{}, numbers)
 
 	recv.Tick(10)
@@ -276,18 +278,28 @@ func TestRecvACK(t *testing.T) {
 	require.Equal(t, uint32(6), seqNAKTo)
 	require.Equal(t, uint32(9), recv.maxSeenSequenceNumber.Val())
 	require.Equal(t, uint32(5), recv.lastACKSequenceNumber.Inc().Val())
+	require.Equal(t, uint32(0), recv.lastDeliveredSequenceNumber.Inc().Val())
 	require.Exactly(t, []uint32{}, numbers)
 
 	recv.Tick(20)
 
-	require.Equal(t, uint32(10), seqACK)
+	require.Equal(t, uint32(5), seqACK)
 	require.Equal(t, uint32(5), seqNAKFrom)
 	require.Equal(t, uint32(6), seqNAKTo)
 	require.Equal(t, uint32(9), recv.maxSeenSequenceNumber.Val())
-	require.Equal(t, uint32(10), recv.lastACKSequenceNumber.Inc().Val())
+	require.Equal(t, uint32(5), recv.lastACKSequenceNumber.Inc().Val())
+	require.Equal(t, uint32(5), recv.lastDeliveredSequenceNumber.Inc().Val())
 	require.Exactly(t, []uint32{0, 1, 2, 3, 4}, numbers)
 
 	recv.Tick(30)
+
+	require.Equal(t, uint32(5), seqACK)
+	require.Equal(t, uint32(5), seqNAKFrom)
+	require.Equal(t, uint32(6), seqNAKTo)
+	require.Equal(t, uint32(9), recv.maxSeenSequenceNumber.Val())
+	require.Equal(t, uint32(5), recv.lastACKSequenceNumber.Inc().Val())
+	require.Equal(t, uint32(5), recv.lastDeliveredSequenceNumber.Inc().Val())
+	require.Exactly(t, []uint32{0, 1, 2, 3, 4}, numbers)
 
 	for i := 5; i < 7; i++ {
 		p := packet.NewPacket(addr)
@@ -297,13 +309,6 @@ func TestRecvACK(t *testing.T) {
 		recv.Push(p)
 	}
 
-	require.Equal(t, uint32(5), seqACK)
-	require.Equal(t, uint32(5), seqNAKFrom)
-	require.Equal(t, uint32(6), seqNAKTo)
-	require.Equal(t, uint32(9), recv.maxSeenSequenceNumber.Val())
-	require.Equal(t, uint32(5), recv.lastACKSequenceNumber.Inc().Val())
-	require.Exactly(t, []uint32{0, 1, 2, 3, 4}, numbers)
-
 	recv.Tick(40)
 
 	require.Equal(t, uint32(10), seqACK)
@@ -311,6 +316,7 @@ func TestRecvACK(t *testing.T) {
 	require.Equal(t, uint32(6), seqNAKTo)
 	require.Equal(t, uint32(9), recv.maxSeenSequenceNumber.Val())
 	require.Equal(t, uint32(10), recv.lastACKSequenceNumber.Inc().Val())
+	require.Equal(t, uint32(10), recv.lastDeliveredSequenceNumber.Inc().Val())
 	require.Exactly(t, []uint32{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, numbers)
 }
 
@@ -540,4 +546,79 @@ func TestSkipTooLate(t *testing.T) {
 
 	require.Equal(t, uint32(13), seqACK)
 	require.Equal(t, []uint32{0, 1, 2, 3, 4, 8, 9}, numbers)
+}
+
+func TestIssue67(t *testing.T) {
+	ackNumbers := []uint32{}
+	nakNumbers := [][2]uint32{}
+	numbers := []uint32{}
+	recv := mockLiveRecv(
+		func(seq circular.Number, light bool) {
+			ackNumbers = append(ackNumbers, seq.Val())
+		},
+		func(from, to circular.Number) {
+			nakNumbers = append(nakNumbers, [2]uint32{from.Val(), to.Val()})
+		},
+		func(p packet.Packet) {
+			numbers = append(numbers, p.Header().PacketSequenceNumber.Val())
+		},
+	)
+
+	addr, _ := net.ResolveIPAddr("ip", "127.0.0.1")
+
+	p := packet.NewPacket(addr)
+	p.Header().PacketSequenceNumber = circular.New(0, packet.MAX_SEQUENCENUMBER)
+	p.Header().PktTsbpdTime = 1
+
+	recv.Push(p)
+
+	recv.Tick(10)
+	recv.Tick(20)
+	recv.Tick(30)
+	recv.Tick(40)
+	recv.Tick(50)
+	recv.Tick(60)
+	recv.Tick(70)
+	recv.Tick(80)
+	recv.Tick(90)
+
+	require.Equal(t, []uint32{1, 1, 1, 1, 1, 1, 1, 1, 1}, ackNumbers)
+
+	p = packet.NewPacket(addr)
+	p.Header().PacketSequenceNumber = circular.New(12, packet.MAX_SEQUENCENUMBER)
+	p.Header().PktTsbpdTime = 121
+
+	recv.Push(p)
+
+	require.Equal(t, [][2]uint32{
+		{1, 11},
+	}, nakNumbers)
+
+	p = packet.NewPacket(addr)
+	p.Header().PacketSequenceNumber = circular.New(1, packet.MAX_SEQUENCENUMBER)
+	p.Header().PktTsbpdTime = 11
+
+	recv.Push(p)
+
+	p = packet.NewPacket(addr)
+	p.Header().PacketSequenceNumber = circular.New(11, packet.MAX_SEQUENCENUMBER)
+	p.Header().PktTsbpdTime = 111
+
+	recv.Push(p)
+
+	recv.Tick(100)
+
+	require.Equal(t, []uint32{1, 1, 1, 1, 1, 1, 1, 1, 1, 2}, ackNumbers)
+
+	recv.Tick(110)
+
+	require.Equal(t, []uint32{1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2}, ackNumbers)
+
+	recv.Tick(120)
+
+	require.Equal(t, []uint32{1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 13}, ackNumbers)
+
+	recv.Tick(130)
+
+	require.Equal(t, []uint32{1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 13, 13}, ackNumbers)
 }
